2c70a17ba9ee7f6e2af9fadc17a08d239c4e5ea9
Ilham Ramadhani <ilhamgilang3110@gmail.com>
2026-06-16 22:49:16 +0800

    fix: change default ALLOWED_HOSTS to '*' in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
